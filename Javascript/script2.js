220961b4d196bdd5e7fb41c4eca8ca27210f9fe9
Prajwal <prajwalchaulagai098@gmail.com>
2024-12-04 08:09:21 +0545

    DOM added

---
+++ Javascript/script2.js
@@ -0,0 +1,43 @@
+
+const head = document.getElementById("heading");
+console.log(head)
+const paragraph = document.getElementById("paragraph");
+
+const highlightButton = document.querySelector("#highlight");
+const toggleClass = document.querySelector("#toggle-class-button");
+const addItem = document.querySelector("#add-list-item");
+const removeItem = document.querySelector("#remove-list-item");
+
+const lists = document.querySelector("#lists");
+
+highlightButton.addEventListener("click", () => {
+
+    head.classList.add("highlighted")
+    paragraph.classList.add("highlighted")
+
+});
+
+toggleClass.addEventListener("click",()=>{
+    head.classList.remove("highlighted")
+    paragraph.classList.remove("highlighted")
+
+})
+
+
+addItem.addEventListener("click", () => {
+
+    const item = document.createElement("li");
+    item.textContent =`Item ${lists.childElementCount + 1}`
+    lists.appendChild(item);
+
+})
+
+
+removeItem.addEventListener("click", () => {
+
+
+    const item = lists.lastChild;
+    if (item) item.remove()
+
+
+})
